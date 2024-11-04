9b8ab5c139d039e3da65bdeaa1306306663af742
Isildil <raquel.difraxion@gmail.com>
2024-11-03 20:53:21 -0600

    Update bank_targeted_business_questions.sql

--- bank_targeted_business_questions.sql
+++ bank_targeted_business_questions.sql
@@ -44,13 +44,62 @@ SELECT AVG(duration)/60 AS 'average duration'
 FROM term_deposit.bank_full
 ;
 
+
 -- average handle time
-SELECT y, AVG(duration)/60 AS 'average handle time'
+SELECT (SUM(duration)/ (SELECT COUNT(*) FROM term_deposit.bank_full WHERE y='yes'))/60 AS 'average handle time'
 FROM term_deposit.bank_full
-GROUP BY y
+
 ;
 
+-- number and percentage of clients by housing loan
+SELECT housing, COUNT(housing) AS total, 
+COUNT(housing)*100/(SELECT COUNT(*) FROM term_deposit.bank_full) AS percentage
+FROM term_deposit.bank_full
+GROUP BY housing
+;
 
+-- number and percentage of clients by job
+SELECT job, COUNT(housing) AS total, 
+COUNT(job)*100/(SELECT COUNT(*) FROM term_deposit.bank_full) AS percentage
+FROM term_deposit.bank_full
+GROUP BY job
+ORDER BY job
+;
 
+-- Conversion rate by job
+SELECT *
+FROM (
+	SELECT job,y, COUNT(y)*100/SUM(campaign) AS 'conversion rate'
+	FROM term_deposit.bank_full
+	GROUP BY y, job
+    ORDER BY job) as t
+WHERE y='yes'
+;
 
+-- conversion rate by month
+SELECT *
+FROM (
+	SELECT month,y, COUNT(y)*100/SUM(campaign) AS 'conversion rate'
+	FROM term_deposit.bank_full
+	GROUP BY y, month
+    ORDER BY month) as t
+WHERE y='yes'
+;
 
+-- conversion rate by housing loan
+SELECT *
+FROM (
+	SELECT housing,y, COUNT(y)*100/SUM(campaign) AS 'conversion rate'
+	FROM term_deposit.bank_full
+	GROUP BY y, housing
+    ORDER BY housing) as t
+WHERE y='yes'
+;
+-- conversion rate by previous campaign success
+SELECT *
+FROM (
+	SELECT poutcome, y, COUNT(y)*100/SUM(campaign) AS 'conversion rate'
+	FROM term_deposit.bank_full
+	GROUP BY y, poutcome) as t
+WHERE y='yes'
+;
